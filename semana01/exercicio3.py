5225fda01468461366dd13eb2dd29e56b645b93b
matheusbrivio <matheusbrivio22@gmail.com>
2025-03-25 23:38:02 -0300

    Create exercicio3.py

---
+++ semana01/exercicio3.py
@@ -0,0 +1,5 @@
+Numero = float(input("Insira o numero:"))
+DobroDoNumero = Numero * 2
+MetadeDoNumero = Numero / 2
+print("Dobro do numero:", DobroDoNumero)
+print("Metade do numero", MetadeDoNumero)
