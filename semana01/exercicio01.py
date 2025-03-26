92c47955ba85e389db6bf3fb1f1cbd13ac740448
matheusbrivio <matheusbrivio22@gmail.com>
2025-03-25 23:36:16 -0300

    Create exercicio01.py

---
+++ semana01/exercicio01.py
@@ -0,0 +1,5 @@
+NumeroDeCavalos = int(input("Insira o numero de cavalos"))
+ValorFerraduras = 80
+TotalFerraduras = NumeroDeCavalos * 4
+ValorTotalFerraduras = TotalFerraduras * 80
+print("Valor para troca de ferraduras:", ValorTotalFerraduras)
