ca3421b198078f49b03376cb6cfd3d034dd011f7
matheusbrivio <matheusbrivio22@gmail.com>
2025-03-25 23:50:44 -0300

    Create exercicio5.py

---
+++ semana01/exercicio5.py
@@ -0,0 +1,5 @@
+pneus = 395.40
+numeroCarros = int(input("Insira o numero de carros"))
+ValorPorCarro = pneus * 4
+ValorfinalDaTroca = ValorPorCarro * numeroCarros
+print("O valor para troca de pneus é:", ValorfinalDaTroca)
