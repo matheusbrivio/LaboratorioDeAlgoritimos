8025f2b34aa2ae15ecf35e90bd899e073d6b635c
matheusbrivio <matheusbrivio22@gmail.com>
2025-03-26 13:44:45 -0300

    Create exercicio4.py

---
+++ semana3/exercicio4.py
@@ -0,0 +1,4 @@
+ValorTanque = float(input("Insira o valor desejado"))
+ValorGasolina = float(input("Insira o valor desejado"))
+LitrosTanque = ValorTanque / ValorGasolina
+print("Total de litros:",LitrosTanque)
