4146f7dce9a1649d14e159aff9bd7a4f6417bc87
matheusbrivio <matheusbrivio22@gmail.com>
2025-04-01 12:59:29 -0300

    Update exercicio4.py

--- semana4/exercicio4.py
+++ semana4/exercicio4.py
@@ -1,1 +1,14 @@
+Nota1 = float(input("Insira a primeira nota do aluno:"))
+Nota2 = float(input("Insira a segunda nota do aluno:"))
+Media = (Nota1 + Nota2) / 2
 
+if Media >= 9 and Media <= 10:
+    print("O aluno teve as notas:", Nota1, Nota2, "Media:",Media, "E conceito: A, Portando APROVADO.")
+elif Media >= 7.5 and Media <= 9:
+    print("O aluno teve as notas:", Nota1, Nota2, "Media:",Media, "E conceito: B, Portando APROVADO.")
+elif Media >= 6 and Media <= 7.5:
+    print("O aluno teve as notas:", Nota1, Nota2, "Media:",Media, "E conceito: C, Portando APROVADO.")
+elif Media >= 4 and Media <= 6:
+    print("O aluno teve as notas:", Nota1, Nota2, "Media:",Media, "E conceito: D, Portando REPROVADO.")
+else: 
+    print("O aluno teve as notas:", Nota1, Nota2, "Media:",Media, "E conceito: E, Portando REPROVADO.")
