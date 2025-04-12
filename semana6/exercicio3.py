f8bb3ec69ebb68274c973244cf8e251c6a133bc0
matheusbrivio <matheusbrivio22@gmail.com>
2025-04-12 18:48:18 -0300

    Update exercicio3.py

--- semana6/exercicio3.py
+++ semana6/exercicio3.py
@@ -1,1 +1,7 @@
-;
+numero = int(input("Digite um numero:"))
+contador = 0
+while numero != 0:
+    if numero == 10:
+        contador += 1
+    numero = int(input("Digite um numero:"))
+print ("O numero de numeros 10 foi repetido:",contador, "vezes")
