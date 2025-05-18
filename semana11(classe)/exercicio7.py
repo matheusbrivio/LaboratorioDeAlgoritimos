eaa8275b07cd4cda5b8aca1f258977d8134f1f5d
matheusbrivio <matheusbrivio22@gmail.com>
2025-05-18 18:20:51 -0300

    Update exercicio7.py

--- semana11(classe)/exercicio7.py
+++ semana11(classe)/exercicio7.py
@@ -1,1 +1,11 @@
+import random
+numeros = []
+
+for i in range (10):
+    numero = random.randint(1,100)
+    numeros.append(numero)
+
+for i in range (10):
+    if numeros[i] % 2 == 0:
+        print(numeros[i])
 
